e6f1f48fd951134ca3ef7a6d03ca1950eb4c5d1a
JinwooJeong <jjwlove12@kpu.ac.kr>
2021-05-25 17:05:20 +0900

    login 구현 + 코드 설명 추가

--- main.c
+++ main.c
@@ -18,15 +18,22 @@ dual time - 미국/한국 시간 이런식 =? GMT+N
 #define LCD_DATA (*(volatile unsigned char *)0x8000)
 #define LCD_CTL (*(volatile unsigned char *)0x8000)
 
+
+//상태
+#define STAT_INIT 0
+#define STAT_LOGIN 1
+#define STAT_TIME 2
+#define STAT_DTIME 3
+
 volatile int cnt = 0, led=0;
 unsigned char keydata = 0;
 unsigned char key_old = 0;
-unsigned int pw[4] = {1,2,3,4};
-unsigned int input_pw[4] = {0,0,0,0};
+unsigned int pw = 0;
+unsigned int input_pw = 0;
 unsigned int input_cnt = 0;
 
 
-
+unsigned int status = 0;
 
 void port_init(void)
 { PORTA = 0x00;
@@ -50,49 +57,61 @@ void login(){
     keydata = PINB & 0xff;
     if(keydata != key_old){ //처음 값이 들어올떄
         if(input_cnt < 4){
+            
 		    switch(keydata){
                 case 0x01:
-                
-                input_pw[input_cnt++] = 0;
+                input_pw *= 10;
+                input_pw += 0;
+                input_cnt ++;
                 break;
                 case 0x02:
-                input_pw[input_cnt++] = 1;
+                input_pw *= 10;
+                input_pw += 1;
+                input_cnt ++;
                 break;
                 case 0x04:
-                input_pw[input_cnt++] = 2;
+                input_pw *= 10;
+                input_pw += 2;
+                input_cnt ++;
                 break;
                 case 0x08:
-                input_pw[input_cnt++] = 3;
+                input_pw *= 10;
+                input_pw += 3;
+                input_cnt ++;
                 break;
                 case 0x10:
-                input_pw[input_cnt++] = 4;
+                input_pw *= 10;
+                input_pw += 4;
+                input_cnt ++;
                 break;
                 case 0x20:
-                input_pw[input_cnt++] = 5;
+                input_pw *= 10;
+                input_pw += 5;
+                input_cnt ++;
                 break;
                 case 0x40:
-                input_pw[input_cnt++] = 6;
+                input_pw *= 10;
+                input_pw += 6;
+                input_cnt ++;
                 break;
                 case 0x80:
-                input_pw[input_cnt++] = 7;   
+                input_pw *= 10;
+                input_pw += 7;   
+                input_cnt ++;
                 break;
                 default :
                 break;
             }
+            _delay_ms(500);
+            
         }else{
-            int num = 1;
-            for(int i = 0; i < 4; i++){
-                if(pw[i] != input_pw[i]){
-                    num = 0;
-                }
-            }
-            if(num != 1)
-            {
+                    
+            if(input_pw != pw){
                 input_cnt = 0;
-                for(int i = 0; i < 4; i++)
-                {
-                    input[i] = 0;
-                }
+                input_pw = 0;
+            }
+            else{
+                status = STAT_TIME;
             }
 
         }
@@ -101,13 +120,31 @@ void login(){
     
 }
 
-void lcdCTL(){
-	lcd_gotoxy(1,1);
-	lcd_putn3(led);
+void lcdCTL(int stat){
+	//lcd_gotoxy(1,1);
+	//lcd_putn3(led);
+    switch(stat){
+        case STAT_LOGIN:
+        lcd_gotoxy(1,1);
+        lcd_putss("input password");
+        lcd_gotoxy(1,2);
+        lcd_putn4(input_pw);
+        lcd_gotoxy(6,2);
+        lcd_putn4(pw);
+        break;
+        case STAT_TIME:
+        lcd_gotoxy(1,1);
+        lcd_putn4(led);
+        lcd_gotoxy(1,2);
+        lcd_putss("login complete");
+            break;
+        default :
+            break;
+    }
 	//lcd_gotoxy(1,2);
     //lcd_putn4(keydata);
-    lcd_gotoxy(6,2);
-    lcd_putn4(input_pw);
+    //lcd_gotoxy(6,2);
+    //lcd_putn4(input_pw);
 }
 
 ISR(TIMER0_COMP_vect){ 
@@ -120,6 +157,18 @@ ISR(TIMER0_COMP_vect){
     }
 }
 
+void manager(unsigned int stat){
+
+    lcdCTL(stat);
+    switch(stat){
+        case STAT_LOGIN:
+        login();
+        break;
+        case STAT_TIME:
+        break;
+
+    }
+}
 
 void main(void){ 
     port_init();
@@ -132,8 +181,9 @@ void main(void){
     SREG = 0b10000000;
     lcdInit();
     lcdClear();
+    status = STAT_LOGIN;
+
     while(1){
-        lcdCTL();
-        login();
+        manager(status);
 	};
 }
